d50f038696b4ee860e5ceeb8399d3da4f962f242
DrSavinkov <dr.savinkov@yandex.ru>
2013-10-05 19:38:45 +0400

    It's a root.

---
+++ afc.h
@@ -0,0 +1,4 @@
+#include <stdlib.h>
+#include <random>
+
+long double random(){return (long double(rand()))/(long double(RAND_MAX));}
---
+++ dia5.cpp
@@ -0,0 +1,20 @@
+// DIA5.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+#include <stdlib.h>
+#include "classes.h"
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+vector
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	cout << "In 1 Gb can be ~" << (1024*1024*1024)/sizeof(T_cell) << endl;
+	int c;
+	cin >> c;
+	return 0;
+}
+
